76a4ff071ade274c14ad43c2147c1ab12eed7ae5
Nick Barry <itsnickbarry@protonmail.ch>
2018-11-09 13:48:27 -0600

    add green apple, strawberry, and mango

--- set_fruit.sh
+++ set_fruit.sh
@@ -1,3 +1,3 @@
-AVAILABLE_FRUITS="🍎🍐🍊🍋🍇🍈🍒🍑🍅"
+AVAILABLE_FRUITS="🍏🍎🍐🍊🍋🍓🍇🍈🍒🍑🍅🥭"
 CURRENT_FRUIT=${AVAILABLE_FRUITS:$(( RANDOM  % ${#AVAILABLE_FRUITS} )):1}
 PS1=$(echo $PS1 | sed "s/[\\\\]\?.$/$CURRENT_FRUIT /")
