ce93e437c5c7bc80106509afb9a99d491379d446
Nick Barry <itsnickbarry@protonmail.ch>
2021-04-11 18:33:49 -0500

    add kiwi, watermelon, mango, coconut, and pineapple

--- set_fruit.sh
+++ set_fruit.sh
@@ -1,4 +1,4 @@
 
-AVAILABLE_FRUITS="🍏🍎🍐🍊🍋🍓🍇🍈🍒🍑🍅"
+AVAILABLE_FRUITS="🍏🍎🍐🍊🍋🍓🍇🍈🍒🍑🍅🥝🍉🥭🥥🍍"
 CURRENT_FRUIT=${AVAILABLE_FRUITS:$(( RANDOM  % ${#AVAILABLE_FRUITS} )):1}
 PS1=$(echo $PS1 | sed "s/[\\\\]\?.$/$CURRENT_FRUIT /")
